4b15743884e8a32fb7ca262faed8935ae68b52a7
Will Fong <thewillfong@gmail.com>
2022-01-16 20:53:10 -0800

    added initial project structure and files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Instacart’s data science team plays a big part in providing this delightful shopping experience. Currently they use transactional data to develop models that predict which products a user will buy again, try for the first time, or add to their cart next during a session. ',
+    author='William Fong',
+    license='MIT',
+)
